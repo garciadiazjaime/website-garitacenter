da70e0f14bfbd95b557b9a0c808e103480f4dc99
jaime <garciadiazjaime@gmail.com>
2016-07-07 20:22:07 -0500

    show ads

--- src/shared/utils/string/index.js
+++ src/shared/utils/string/index.js
@@ -21,3 +21,29 @@ export function toTitleCase(data) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
 }
+
+export function timeSince(data) {
+  const seconds = Math.floor((new Date() - new Date(data)) / 1000);
+  let interval = Math.floor(seconds / 31536000);
+
+  if (interval > 1) {
+    return `${interval} años`;
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return `${interval} meses`;
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return `${interval} días`;
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return `${interval} horas`;
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return `${interval} minutos`;
+  }
+  return `${Math.floor(seconds)} segundos`;
+}
