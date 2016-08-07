08d0d48ac160c7ce1fb04291863700ca53fc2975
jaime <garciadiazjaime@gmail.com>
2016-08-07 13:07:28 -0500

    reporte usuario js logic

--- src/shared/components/sections/reporteUsuario/formReporteUsuario.js
+++ src/shared/components/sections/reporteUsuario/formReporteUsuario.js
@@ -1,8 +1,40 @@
 /* eslint max-len: [2, 500, 4] */
 import React from 'react';
+import _ from 'lodash';
 
+import ClickOption from './clickOption';
+import { toTitleCase } from '../../../utils/string';
 
 export default class FormReporteUsuario extends React.Component {
+
+  constructor() {
+    super();
+    this.clickHandler = this.clickHandler.bind(this);
+    this.submitHandler = this.submitHandler.bind(this);
+    this.clickHandler = this.clickHandler.bind(this);
+    this.state = {};
+  }
+
+  getLabel(prop) {
+    if (prop === 'port') {
+      return !_.isEmpty(this.state.port) ? toTitleCase(this.state.port) : 'Seleccionar Garita';
+    } else if (prop === 'place') {
+      return !_.isEmpty(this.state.place) ? toTitleCase(this.state.place) : 'Seleccionar Lugar';
+    } else if (prop === 'time') {
+      return !_.isEmpty(this.state.time) ? toTitleCase(this.state.time) : 'Seleccionar Tiempo';
+    }
+    return '';
+  }
+
+  submitHandler() {
+    console.log(this.state);
+  }
+
+  clickHandler(option, value) {
+    const state = _.assign({}, this.state);
+    state[option] = value;
+    this.setState(state);
+  }
 
   render() {
     return (<div className="container-fluid">
@@ -17,28 +49,27 @@ export default class FormReporteUsuario extends React.Component {
           <br />
           <div className="btn-group">
             <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Seleccionar Garita <span className="caret"></span>
+              {this.getLabel('port')} <span className="caret"></span>
             </button>
             <ul className="dropdown-menu">
-              <li><a href="#">San Ysidro</a></li>
-              <li><a href="#">Otay</a></li>
+            <li><ClickOption prop="port" value="san_ysidro" clickHandler={this.clickHandler}>San Ysidro</ClickOption></li>
+            <li><ClickOption prop="port" value="otay" clickHandler={this.clickHandler}>Otay</ClickOption></li>
             </ul>
           </div>
         </div>
-
         <div className="form-group">
           <label htmlFor="exampleInputPassword1">Dónde estas?</label>
           <br />
           <div className="btn-group">
             <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Seleccionar Lugar <span className="caret"></span>
+              {this.getLabel('place')} <span className="caret"></span>
             </button>
             <ul className="dropdown-menu">
-              <li><a href="#">Lugar A</a></li>
-              <li><a href="#">Lugar B</a></li>
-              <li><a href="#">Lugar C</a></li>
-              <li><a href="#">Lugar D</a></li>
-              <li><a href="#">Lugar E</a></li>
+              <li><ClickOption prop="place" value="place_a" clickHandler={this.clickHandler}>Place A</ClickOption></li>
+              <li><ClickOption prop="place" value="place_b" clickHandler={this.clickHandler}>Place B</ClickOption></li>
+              <li><ClickOption prop="place" value="place_c" clickHandler={this.clickHandler}>Place C</ClickOption></li>
+              <li><ClickOption prop="place" value="place_d" clickHandler={this.clickHandler}>Place D</ClickOption></li>
+              <li><ClickOption prop="place" value="place_e" clickHandler={this.clickHandler}>Place E</ClickOption></li>
             </ul>
           </div>
         </div>
@@ -47,18 +78,18 @@ export default class FormReporteUsuario extends React.Component {
           <br />
           <div className="btn-group">
             <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Seleccionar Tiempo <span className="caret"></span>
+              {this.getLabel('time')} <span className="caret"></span>
             </button>
             <ul className="dropdown-menu">
-              <li><a href="#">Tiempo A</a></li>
-              <li><a href="#">Tiempo B</a></li>
-              <li><a href="#">Tiempo C</a></li>
-              <li><a href="#">Tiempo D</a></li>
-              <li><a href="#">Tiempo E</a></li>
+            <li><ClickOption prop="time" value="time_a" clickHandler={this.clickHandler}>Tiempo A</ClickOption></li>
+            <li><ClickOption prop="time" value="time_b" clickHandler={this.clickHandler}>Tiempo B</ClickOption></li>
+            <li><ClickOption prop="time" value="time_c" clickHandler={this.clickHandler}>Tiempo C</ClickOption></li>
+            <li><ClickOption prop="time" value="time_d" clickHandler={this.clickHandler}>Tiempo D</ClickOption></li>
+            <li><ClickOption prop="time" value="time_e" clickHandler={this.clickHandler}>Tiempo E</ClickOption></li>
             </ul>
           </div>
         </div>
-        <button className="btn btn-default">Submit</button>
+        <button className="btn btn-default" onClick={this.submitHandler}>Submit</button>
       </div>
     </div>);
   }
