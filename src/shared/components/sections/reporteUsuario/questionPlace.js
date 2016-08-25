815c3a6cf75bdaca2fc91705ec5c812f8aecb3ac
jaime <garciadiazjaime@gmail.com>
2016-08-24 22:06:24 -0500

    move save report to review view

--- src/shared/components/sections/reporteUsuario/questionPlace.js
+++ src/shared/components/sections/reporteUsuario/questionPlace.js
@@ -8,6 +8,8 @@ export default class QuestionPlace extends React.Component {
     super();
     this.clickHandler = this.clickHandler.bind(this);
     this.backHandler = this.backHandler.bind(this);
+    this.renderSanYsidrio = this.renderSanYsidrio.bind(this);
+    this.renderOtay = this.renderOtay.bind(this);
   }
 
   clickHandler(value) {
@@ -25,36 +27,73 @@ export default class QuestionPlace extends React.Component {
     });
   }
 
-  render() {
-    return (<div className="container-fluid">
-      A qué altura estas?
-      <div className="row">
-        <div className="col-sm-12">
-          <ClickOption value="place_a" clickHandler={this.clickHandler}>
-            A menos de 10 carros
-          </ClickOption>
-        </div>
-        <div className="col-sm-12">
-          <ClickOption value="place_b" clickHandler={this.clickHandler}>
-            En el puente
-          </ClickOption>
-        </div>
-        <div className="col-sm-12">
-          <ClickOption value="place_c" clickHandler={this.clickHandler}>
-            En la frutería
-          </ClickOption>
-        </div>
-        <div className="col-sm-12">
-          <ClickOption value="place_d" clickHandler={this.clickHandler}>
-            En los mariscos
-          </ClickOption>
-        </div>
-        <div className="col-sm-12">
-          <ClickOption value="place_e" clickHandler={this.clickHandler}>
-            En el parque de la amistad
-          </ClickOption>
-        </div>
+  renderSanYsidrio() {
+    return (<div className="row">
+      <div className="col-sm-12">
+        <ClickOption value="place_a" clickHandler={this.clickHandler}>
+          A menos de 10 carros
+        </ClickOption>
       </div>
+      <div className="col-sm-12">
+        <ClickOption value="place_b" clickHandler={this.clickHandler}>
+          En el puente
+        </ClickOption>
+      </div>
+      <div className="col-sm-12">
+        <ClickOption value="place_c" clickHandler={this.clickHandler}>
+          En las tiendas
+        </ClickOption>
+      </div>
+      <div className="col-sm-12">
+        <ClickOption value="place_d" clickHandler={this.clickHandler}>
+          En el arco
+        </ClickOption>
+      </div>
+      <div className="col-sm-12">
+        <ClickOption value="place_e" clickHandler={this.clickHandler}>
+          En costco
+        </ClickOption>
+      </div>
+    </div>);
+  }
+
+  renderOtay() {
+    return (<div className="row">
+      <div className="col-sm-12">
+        <ClickOption value="place_a" clickHandler={this.clickHandler}>
+          A menos de 10 carros
+        </ClickOption>
+      </div>
+      <div className="col-sm-12">
+        <ClickOption value="place_b" clickHandler={this.clickHandler}>
+          En el puente
+        </ClickOption>
+      </div>
+      <div className="col-sm-12">
+        <ClickOption value="place_c" clickHandler={this.clickHandler}>
+          En la frutería
+        </ClickOption>
+      </div>
+      <div className="col-sm-12">
+        <ClickOption value="place_d" clickHandler={this.clickHandler}>
+          En los mariscos
+        </ClickOption>
+      </div>
+      <div className="col-sm-12">
+        <ClickOption value="place_e" clickHandler={this.clickHandler}>
+          En el parque de la amistad
+        </ClickOption>
+      </div>
+    </div>);
+  }
+
+  render() {
+    const { port } = this.props;
+    return (<div className="container-fluid">
+      <div className="row">
+        A qué altura estas?
+      </div>
+      { port === 'san_ysidro' ? this.renderSanYsidrio() : this.renderOtay() }
       <div className="row">
         <a onClick={this.backHandler}>Volver</a>
       </div>
@@ -64,4 +103,5 @@ export default class QuestionPlace extends React.Component {
 
 QuestionPlace.propTypes = {
   clickHandler: React.PropTypes.func.isRequired,
+  port: React.PropTypes.string.isRequired,
 };
