907f31e81d9e800751f5fe7ddfe1990982cde82d
jaime <garciadiazjaime@gmail.com>
2016-12-17 15:36:34 -0600

    improve MainMenu

--- src/shared/components/layout/menu/menu1/index.js
+++ src/shared/components/layout/menu/menu1/index.js
@@ -2,23 +2,20 @@ import React from 'react';
 import { Link } from 'react-router';
 const style = require('./style.scss');
 
-export default class MainMenu extends React.Component {
-
-  render() {
-    return (<nav className={style.navbar + ' navbar'} id="menu_wrapper">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-sm-6 col-xs-12">
-              <Link className={style.navbarBrand + ' navbar-brand'} to="/">
-                <img src="/images/garita-center-logo.png" alt="GaritaCenter - reporte de garitas" />
-              </Link>
-              <h1>Reporte de Garitas</h1>
-            </div>
-            <div className="col-sm-6 col-xs-12">
-              <h2 className={'navbar-text navbar-right '}>Tijuana / San Diego</h2>
-            </div>
-          </div>
+export default function MainMenu({ city }) {
+  return (<nav className={style.navbar + ' navbar'} id="menu_wrapper">
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-sm-6 col-xs-12">
+          <Link className={style.navbarBrand + ' navbar-brand'} to="/">
+            <img src="/images/garita-center-logo.png" alt="GaritaCenter - reporte de garitas" />
+          </Link>
+          <h1>Reporte de Garitas</h1>
         </div>
-      </nav>);
-  }
+        <div className="col-sm-6 col-xs-12">
+          <h2 className={'navbar-text navbar-right '}>{city}</h2>
+        </div>
+      </div>
+    </div>
+  </nav>);
 }
