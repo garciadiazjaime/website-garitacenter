a0c42dd4724ff6020db0df77d47c5548231d0862
jaime <garciadiazjaime@gmail.com>
2016-12-17 15:40:48 -0600

    improve MenuReport

--- src/shared/components/elements/menus/menuReport/index.js
+++ src/shared/components/elements/menus/menuReport/index.js
@@ -4,28 +4,20 @@ import React from 'react';
 import { Link } from 'react-router';
 const style = require('./style.scss');
 
-export default class MenuReport extends React.Component {
-
-  render() {
-    const { location } = this.props;
-    const reportLocation = 'reporte-usuario';
-    return (<div className={style.mainWrapper}>
-      <div className="container-fluid">
-        <ul className="nav nav-tabs">
-          <li role="presentation" className={location.indexOf(reportLocation) === -1 ? 'active' : ''}>
-            <Link to="/">
-              Reporte Oficial
-            </Link>
-          </li>
-          <li role="presentation" className={location.indexOf(reportLocation) !== -1 ? 'active' : ''}>
-            <Link to="/reporte-usuario">
-              Reporte Usuarios
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </div>);
-  }
+export default function MenuReport({ location }) {
+  const reportLocation = 'reporte-usuario';
+  return (<div className={style.mainWrapper}>
+    <div className="container-fluid">
+      <ul className="nav nav-tabs">
+        <li role="presentation" className={location.indexOf(reportLocation) === -1 ? 'active' : ''}>
+          <Link to="/">Reporte Oficial</Link>
+        </li>
+        <li role="presentation" className={location.indexOf(reportLocation) !== -1 ? 'active' : ''}>
+          <Link to="/reporte-usuario">Reporte Usuarios</Link>
+        </li>
+      </ul>
+    </div>
+  </div>);
 }
 
 MenuReport.propTypes = {
