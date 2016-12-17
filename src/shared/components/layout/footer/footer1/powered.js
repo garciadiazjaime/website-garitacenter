910490759c6cbb3cc64b318f563a2ddc7095eaa0
jaime <garciadiazjaime@gmail.com>
2016-12-17 15:53:54 -0600

    cleanup footer

--- src/shared/components/layout/footer/footer1/powered.js
+++ src/shared/components/layout/footer/footer1/powered.js
@@ -1,34 +1,21 @@
+/* eslint max-len: [2, 500, 4] */
 import React from 'react';
 
 const style = require('./style.scss');
 
-
-export default class Powered extends React.Component {
-
-  render() {
-    const data = [{
-      name: 'POOL',
-      url: 'http://somospool.com',
-      title: 'somos pool',
-    }, {
-      name: 'MINT',
-      url: 'http://mintitmedia.com',
-      title: 'Diseño y Desarrollo Web en Tijuana',
-    }];
-
-    return (<div className={style.powered}>
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-xs-12 col-sm-6">
-              Todos los derechos reservados &copy; GaritaCenter <br />
-              Reporte de garitas para San Ysidro y Otay | Tijuana
-            </div>
-            <div className="col-xs-12 col-sm-6">
-              Un proyecto de:&nbsp;
-              <a href={data[1].url} title={data[1].title} target="_blank">{data[1].name}</a>
-            </div>
+export default function Powered() {
+  return (<div className={style.powered}>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-xs-12 col-sm-6">
+            Todos los derechos reservados &copy; GaritaCenter <br />
+            Reporte de garitas para San Ysidro y Otay | Tijuana
+          </div>
+          <div className="col-xs-12 col-sm-6">
+            Un proyecto de &nbsp;
+            <a href="http://www.mintitmedia.com" title="Diseño y Desarrollo Web en Tijuana" target="_blank">MINT</a>
           </div>
         </div>
-    </div>);
-  }
+      </div>
+  </div>);
 }
