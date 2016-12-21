fb8465d8afe78485b9cea471ee441dcf4f16c6b9
jaime <garciadiazjaime@gmail.com>
2016-12-20 20:40:47 -0600

    cleanup

--- src/shared/components/sections/home/index.js
+++ src/shared/components/sections/home/index.js
@@ -1,19 +1,27 @@
 /* eslint max-len: [2, 500, 4] */
 import React from 'react';
+import _ from 'lodash';
 
 import Block1 from './block1';
 import Block2 from './block2';
-// import Block3 from './block3';
-// <Block3 />
+import { timeSince } from '../../../utils/string';
+const style = require('./style.scss');
 
 
 export default class HomeSection extends React.Component {
 
   render() {
     const { data } = this.props;
+    const { report } = data;
+    const created = _.isArray(report) && report.length && report[0] ? report[0].created : '';
     return (<div>
       <Block1 data={data} />
       <Block2 />
+      <div>
+        <div className={style.lastUpdate}>
+          Reporte de Garitas actualizado hace {timeSince(created)}
+        </div>
+      </div>
     </div>);
   }
 }
