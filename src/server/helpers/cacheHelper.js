559df03cbb998f54845659f4a3ec298789eaaefe
jaime <garciadiazjaime@gmail.com>
2017-03-26 14:46:59 -0500

    implement cache

---
+++ src/server/helpers/cacheHelper.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-param-reassign */
+import mcache from 'memory-cache';
+
+export default function (duration) {
+  return (req, res, next) => {
+    const key = '__express__' + req.originalUrl || req.url;
+    const cachedBody = mcache.get(key);
+    if (cachedBody) {
+      res.send(cachedBody);
+    } else {
+      res.sendResponse = res.send;
+      res.send = (body) => {
+        mcache.put(key, body, duration * 1000);
+        res.sendResponse(body);
+      };
+      next();
+    }
+  };
+}
